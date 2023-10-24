53cf5075483954154550f042d0f304008889c825
La Vinia Parker <laviniaparker@La-Vinias-MacBook-Pro.local>
2023-10-24 19:00:11 -0400

    finished user model and working on user controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,18 @@
 class ApplicationController < ActionController::API
     before_action :snake_case_params
 
+  
+
     private
+
+    def current_user
+        return nil if session[:session_token] == nil 
+        @current_user ||= User.find_by(session_token: session_token[:session_token])
+    end
+
+    def login!(user)
+        
+    end
 
     def snake_case_params
         params.deep_transform_keys!(&:underscore)

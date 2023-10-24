d8186515aca370e0a84fa90d48329e736d7b043d
La Vinia Parker <laviniaparker@La-Vinias-MacBook-Pro.local>
2023-10-24 18:45:20 -0400

    seeded database ands finished user model and auth part 1

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,28 +15,56 @@ class User < ApplicationRecord
 
     before_validation :ensure_session_token
 
+    def self.find_by_credentials(credential, password)
+      if credential.match?(URI::MailTo::EMAIL_REGEXP)
+        user = find_by(email: credential)
+     else
+        user = find_by(username: credential)
+      end
+      return nil unless user
+
+      # Use the authenticate method to check the password
+      if user.authenticate(password)
+        return user # Return the user if the password is correct
+      else
+        return nil # Return nil if the password is incorrect
+      end
+
+    end 
+
+    def reset_session_token!
+    self.session_token = generate_unique_session_token
+    save!
+    self.session_token
+
+    end
+    
+
     private
 
      def exists?(random_token)
+
+      User.where(session_token: random_token).exists?
     
-      return User.find_by( session_token: random_token) 
+      # return User.find_by( session_token: random_token) 
       
      end 
 
-    def generate_unique_session_token
+    # def generate_unique_session_token
 
-      random_token = SecureRandom.base64(64)
+    #   random_token = SecureRandom.base64(64)
 
-      print "I am generating" + random_token
-
-     if 
-      User.exists?(random_token) 
-       generate_unique_session_token() 
-     else 
-      return random_token  
-     end 
-
-     
+    #  if 
+    #   User.exists?(random_token) 
+    #    generate_unique_session_token() 
+    #  else 
+    #   return random_token  
+    #  end 
+     def generate_unique_session_token
+      loop do
+        random_token = SecureRandom.hex(64)
+        break random_token unless self.class.exists?(session_token: random_token)
+      end
 
     end
 
@@ -47,3 +75,4 @@ class User < ApplicationRecord
 
    
 end
+
